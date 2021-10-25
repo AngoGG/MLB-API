5f4794cde43f12db661f5c308553f2125a401516
Ango <ango.gomes@gmail.com>
2021-10-25 20:38:14 +0200

    Added Game model

--- apps/mlb/models.py
+++ apps/mlb/models.py
@@ -4,15 +4,9 @@ from django.db import models
 
 
 class Team(models.Model):
-    id: models.IntegerField = models.IntegerField(
-        verbose_name="ID MLB de l'équipe"
-    )
-    name: models.CharField = models.CharField(
-        verbose_name="Nom complet de l'équipe"
-    )
-    team_name: models.CharField = models.CharField(
-        verbose_name="Nom court de l'équipe"
-    )
+    id: models.IntegerField = models.IntegerField(verbose_name="ID MLB de l'équipe")
+    name: models.CharField = models.CharField(verbose_name="Nom complet de l'équipe")
+    team_name: models.CharField = models.CharField(verbose_name="Nom court de l'équipe")
     location_name: models.CharField = models.CharField(
         verbose_name="Localisation de l'équipe"
     )
@@ -25,6 +19,25 @@ class Team(models.Model):
     team_code: models.IntegerField = models.IntegerField(
         verbose_name="Team Code de l'équipe"
     )
-    team_league: models.CharField = models.CharField(
-        verbose_name="League de l'équipe", max_length=50
+    team_league: models.CharField = models.CharField(verbose_name="League de l'équipe")
+
+
+class Game(models.Model):
+    game_pk: models.IntegerField = models.IntegerField(verbose_name="ID du match")
+    official_date: models.DateField = models.DateField(verbose_name="Date du match")
+    home_team: models.ForeignKey = models.ForeignKey(
+        Team, verbose_name="Equipe à domicile", on_delete=models.CASCADE
     )
+    away_team: models.ForeignKey = models.ForeignKey(
+        Team, verbose_name="Equipe à l'extérieur", on_delete=models.CASCADE
+    )
+    winner_team: models.ForeignKey = models.ForeignKey(
+        Team, verbose_name="Vainqueur du match", on_delete=models.CASCADE
+    )
+    home_score: models.IntegerField = models.models.IntegerField(
+        verbose_name="Score de l'équipe à domicile"
+    )
+    away_score: models.IntegerField = models.models.IntegerField(
+        verbose_name="Score de l'équipe à l'éxtérieur"
+    )
+
