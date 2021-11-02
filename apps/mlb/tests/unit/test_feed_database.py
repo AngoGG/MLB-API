bf1c17d6200711e42d9b56b3916231132caeeb33
Ango <ango.gomes@gmail.com>
2021-11-02 22:28:24 +0100

    Updated FeedDatabase.update_game test

--- apps/mlb/tests/unit/test_feed_database.py
+++ apps/mlb/tests/unit/test_feed_database.py
@@ -27,9 +27,31 @@ class TestFeedDatabase:
             "home_score": 1,
             "away_score": 9,
         }
+        home_team_data = {
+            "id": 117,
+            "name": "Houston Astros",
+            "team_name": "Astros",
+            "location_name": "Houston",
+            "abbreviation": "HOU",
+            "link": "/api/v1/teams/117",
+            "team_code": "hou",
+            "team_league": "American League",
+        }
+        away_team_data = {
+            "id": 111,
+            "name": "Boston Red Sox",
+            "team_name": "Red Sox",
+            "location_name": "Boston",
+            "abbreviation": "BOS",
+            "link": "/api/v1/teams/11",
+            "team_code": "bos",
+            "team_league": "American League",
+        }
 
         feed_database: FeedDatabase = FeedDatabase()
-        feed_database.update_game(game_data)
+        home_team = Team.objects.create(**home_team_data)
+        away_team = Team.objects.create(**away_team_data)
+        feed_database.update_game(game_data, home_team, away_team)
         game_created: QuerySet = Game.objects.first()
 
         assert game_created.game_pk == 660900
